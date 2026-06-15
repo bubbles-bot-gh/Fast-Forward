b55826e2eb0ef19362e44c6c4366654c9526774d
lunei-solei <68037318+LuneiSolei@users.noreply.github.com>
2026-06-15 13:21:44 +0000

    feat: add IOptions.cs

--- BlossomBotGitHub.FastForward/Core/ActionInfo/IOptions.cs
+++ BlossomBotGitHub.FastForward/Core/ActionInfo/IOptions.cs
@@ -2,5 +2,7 @@ namespace BlossomBotGitHub.FastForward.Core.ActionInfo;
 
 public interface IOptions
 {
-    
+    bool AutoMerge { get; }
+    string PostComment { get; }
+    string CustomCommand { get; }
 }
